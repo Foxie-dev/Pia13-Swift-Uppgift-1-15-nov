73e7461027075e0a636332dc09914e453db9f1bc
Tina <Tina.saleh.888@gmail.com>
2024-11-15 17:51:03 +0100

    Initial Commit

---
+++ Pia13 Swift Uppgift 1 15 nov/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pia13 Swift Uppgift 1 15 nov
+//
+//  Created by Elia Johannes on 2024-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pia13 Swift Uppgift 1 15 nov/Pia13_Swift_Uppgift_1_15_novApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pia13_Swift_Uppgift_1_15_novApp.swift
+//  Pia13 Swift Uppgift 1 15 nov
+//
+//  Created by Elia Johannes on 2024-11-15.
+//
+
+import SwiftUI
+
+@main
+struct Pia13_Swift_Uppgift_1_15_novApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
