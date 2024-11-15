87a61c2426921ed107f42d40ab85beeea8509a00
Tina <Tina.saleh.888@gmail.com>
2024-11-16 00:41:53 +0100

    Massa layout

--- Pia13 Swift Uppgift 1 15 nov/ContentView.swift
+++ Pia13 Swift Uppgift 1 15 nov/ContentView.swift
@@ -1,24 +1,80 @@
 //
 //  ContentView.swift
-//  Pia13 Swift Uppgift 1 15 nov
+//  PIA13 Swift Uppgift 1
 //
-//  Created by Elia Johannes on 2024-11-15.
+//  Created by Malmö Yrkehögskolan on 2024-11-05.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+VStack(spacing: 0) {
+                // In the top row (Red, Green, Yellow)
+                HStack(alignment: .top, spacing: 0) {
+                    Color.red
+                        .frame(maxWidth:.infinity, maxHeight: 400)
+                        .padding([.top, .leading, .bottom], 3)
+                    Color.green
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 3)
+                    Color.yellow
+                        .frame(maxWidth: .infinity)
+                        .padding([.top, .bottom, .trailing], 3)
+                }
+                .frame(height: 100)
+    
+    
+                
+                // Gray Rectangle above
+                Color.gray
+                    .frame(height: 90)
+                    .padding([.top, .leading, .trailing], 3.0)
+    
+    
+                
+                // The White Box with blue background
+    
+                ZStack {
+                    Color.white
+                        .frame(width: 100, height: 100)
+                        .padding(.bottom, 422.0)
+                    
+                    
+               
+                .frame(height: 650)
+                // Bottom Row (Red square with Black Square inside locted on bottom right)
+                    Spacer()
+                    
+
+                ZStack(alignment: .bottomTrailing) {
+                    Color.red
+                        .frame(maxWidth: .infinity,maxHeight: 100)
+                        .position(x:200,y:615)
+                    HStack{
+                        Spacer()
+                  
+                    Color.black
+                        .frame(width: 50, height: 50)
+                        .padding(20)
+                }
+                .frame(height: 66)
+                }
+            }
         }
-        .padding()
+        
+     .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(Color.cyan)
+      .ignoresSafeArea()
+        }
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
-#Preview {
-    ContentView()
-}
+
